4de89a2a09684e6f127c763d5f1ae688f4972fdd
Sadman Arif Wamim <sadmanarifwamim11@gmail.com>
2024-10-27 15:16:15 +0600

    Added Login Form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { SignUp } from "./components/custom/signup";
-
+import { Login } from "./components/custom/login";
 function App() {
   
 
   return (
     <>
       <SignUp/>
+      <Login/>
     </>
   );
 }
