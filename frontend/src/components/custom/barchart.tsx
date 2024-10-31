a19e3d9f2da3de006d75278b918bc1c042f8db9b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-10-31 13:05:49 +0600

    ui: enhance barchart ui

--- frontend/src/components/custom/barchart.tsx
+++ frontend/src/components/custom/barchart.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { Bar, BarChart } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 
-import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+import {
+   ChartConfig,
+   ChartContainer,
+   ChartLegend,
+   ChartLegendContent,
+   ChartTooltip,
+   ChartTooltipContent,
+} from "@/components/ui/chart";
 
 const chartData = [
    { month: "January", desktop: 186, mobile: 80 },
@@ -26,8 +33,18 @@ const chartConfig = {
 
 export function BarChartComp() {
    return (
-      <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+      <ChartContainer config={chartConfig} className="min-h-[300px] w-1/2">
          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+               dataKey="month"
+               tickLine={true}
+               tickMargin={5}
+               axisLine={true}
+               tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <ChartLegend content={<ChartLegendContent />} />
             <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
             <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
          </BarChart>
